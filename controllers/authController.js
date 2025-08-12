7cca562b7253f205f763fdac40719fbc5f76ceec
Amna Haq <amnahaqamna83@gmail.com>
2025-08-12 13:13:27 -0700

    improved UI

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const { check } = require("express-validator")
+
 exports.getLogin = (req, res) => {
     res.render("auth/login", { pageTitle: "Login", currentPg: "Login", editing: false, isLoggedIn: false})
 }
@@ -10,5 +12,15 @@ exports.postLogout = (req, res) => {
         res.redirect("/login")
     })
 }
+exports.getSignup = (req, res) => {
+    res.render("auth/signup", { pageTitle: "signup", currentPg: "signup", editing: false, isLoggedIn: false})
+}
+exports.postSignup = (req, res) => {
+    console.log(req.body);
+    res.redirect("/login")
+//   const{firstName,lastName,email,password,confirmPassword,userType,terms}=req.body;
+//   check(firstName).notEmpty().withMessage("First name is required").trim().isLength({min:2}).withMessage("First name must be at least 2 character long").matches(/^[a-zA-Z\s]+$/).withMessage("First name must contain letters")
+}
+
 
 
--- controllers/storeController.js
+++ controllers/storeController.js
@@ -3,12 +3,12 @@ const Home = require("../model/home")
 
 
 exports.getIndex = (req, res, next) => {
-  Home.find().then(registeredHomes => { res.render('store/index', { registeredHomes: registeredHomes, pageTitle: "airbnb Home", currentPg: "home",isLoggedIn: req.isLoggedIn }) }
+  Home.find().then(registeredHomes => { res.render('store/index', { registeredHomes: registeredHomes, pageTitle: "airbnb Home", currentPg: "home", isLoggedIn: req.isLoggedIn }) }
   );
 }
 
 exports.getHomes = (req, res, next) => {
-  Home.find().then(registeredHomes => { res.render('store/homeList', { registeredHomes: registeredHomes, pageTitle: "Home List", currentPg: "Home List" ,isLoggedIn: req.isLoggedIn}) }
+  Home.find().then(registeredHomes => { res.render('store/homeList', { registeredHomes: registeredHomes, pageTitle: "Home List", currentPg: "Home List", isLoggedIn: req.isLoggedIn }) }
   );
 }
 
@@ -19,51 +19,54 @@ exports.getHome = (req, res, next) => {
     if (!home) {
       return res.redirect("/homes")
     }
-    res.render("store/homeDetails", { home: home, pageTitle: home.houseName, currentPg: "Homes",isLoggedIn: req.isLoggedIn });
+    res.render("store/homeDetails", { home: home, pageTitle: home.houseName, currentPg: "Homes", isLoggedIn: req.isLoggedIn });
   })
 
 }
 
 exports.getBookings = (req, res, next) => {
-  Home.find().then(registeredHomes => { res.render('store/bookings', { registeredHomes: registeredHomes, pageTitle: "My Bookings", currentPg: "Bookings" ,isLoggedIn: req.isLoggedIn}) }
+  Home.find().then(registeredHomes => { res.render('store/bookings', { registeredHomes: registeredHomes, pageTitle: "My Bookings", currentPg: "Bookings", isLoggedIn: req.isLoggedIn }) }
   );
 }
 
 exports.getFavourites = (req, res, next) => {
-// Referenced documents ko full detail ke sath fetch karta hai. populate: Ek document ke andar reference ID (usually ObjectId) ko uske actual document data se replace kar dena.
-Favourite.find()
-.populate("homeId")
-.then((favourites)=>{
-  // console.log("favourites....",favourites);
-  //getting only homeId from full obj as populate return complete obj
-  const favouriteHomes = favourites.map(fav => fav.homeId);
-   res.render('store/favouriteList', { favouriteHomes: favouriteHomes, pageTitle: "My Favourites", currentPg: "Favourites" ,isLoggedIn: req.isLoggedIn})
-})}
+  // Referenced documents ko full detail ke sath fetch karta hai. populate: Ek document ke andar reference ID (usually ObjectId) ko uske actual document data se replace kar dena.
+  Favourite.find()
+    .populate("homeId")
+    .then((favourites) => {
+      console.log("favourites....",favourites);
+      //getting only homeId from full obj as populate return complete obj
+      const favouriteHomes = favourites.map(fav => fav.homeId);
+      res.render('store/favouriteList', { favouriteHomes: favouriteHomes, pageTitle: "My Favourites", currentPg: "Favourites", isLoggedIn: req.isLoggedIn })
+    })
+}
 
 exports.PostFavourites = (req, res, next) => {
   const homeId = req.body.homeId;
   // console.log(homeId);
   Favourite.findOne({ homeId: homeId }).then(existingFavourite => {
-    if(existingFavourite) {
+    if (existingFavourite) {
       console.log("Home already in favourites");
-    }else{
+      res.redirect("/favourites")
+
+    } else {
       const newFavourite = new Favourite({ homeId: homeId });
-       newFavourite.save().then(() => {
-         res.redirect("/favourites")
-       });
+      newFavourite.save().then(() => {
+        res.redirect("/favourites")
+      });
     }
   })
 }
 
 exports.DeleteFavourites = (req, res, next) => {
   const homeId = req.params.homeId;
-  Favourite.findOneAndDelete({homeId: homeId})
-  .then(()=>{
-  res.redirect("/favourites")
-  }) 
-  .catch(err => {
-    console.log("Error while deleting favourite: ", err);
-  });
+  Favourite.findOneAndDelete({ homeId: homeId })
+    .then(() => {
+      res.redirect("/favourites")
+    })
+    .catch(err => {
+      console.log("Error while deleting favourite: ", err);
+    });
 
 }
 
